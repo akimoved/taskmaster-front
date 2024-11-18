6b451bff3c4266b0e187a2f1f1d590ed4330f72e
Egor Akimov <akimoved@gmail.com>
2024-11-18 22:42:06 +0400

    add all components for displaying project status

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ProjectComponent } from './project/project.component';
+import { ProjectTitleComponent } from './project/project-title/project-title.component';
+import { TaskListComponent } from './project/task-list/task-list.component';
+import { ProgressBarComponent } from './project/progress-bar/progress-bar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ProjectComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
