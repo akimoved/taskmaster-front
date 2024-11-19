147b731b9c684bf1473f9310f390ca6041ed0caa
Egor Akimov <akimoved@gmail.com>
2024-11-19 23:57:50 +0400

    add task form component

--- src/app/project/task-list/task-list.component.ts
+++ src/app/project/task-list/task-list.component.ts
@@ -2,16 +2,18 @@ import { TaskService } from './../../task.service';
 import { Component, inject } from '@angular/core';
 import { Task } from '../../task.model';
 import { DatePipe } from '@angular/common';
+import { TaskFormComponent } from '../task-form/task-form.component';
 
 @Component({
   selector: 'app-task-list',
   standalone: true,
-  imports: [DatePipe],
+  imports: [DatePipe, TaskFormComponent],
   templateUrl: './task-list.component.html',
   styleUrl: './task-list.component.css'
 })
 export class TaskListComponent {
   tasks: Task[] = [];
+  showModal: boolean = false;
 
   private taskSerivce: TaskService = inject(TaskService);
 
