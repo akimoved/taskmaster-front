685f441df46dea94c417eeb719d17af1290fe317
Egor Akimov <akimoved@gmail.com>
2024-11-19 23:30:41 +0400

    add dynamic loading of task objects into tasklist template

--- src/app/project/task-list/task-list.component.ts
+++ src/app/project/task-list/task-list.component.ts
@@ -1,12 +1,52 @@
 import { Component } from '@angular/core';
+import { Task } from '../../task.model';
+import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-task-list',
   standalone: true,
-  imports: [],
+  imports: [DatePipe],
   templateUrl: './task-list.component.html',
   styleUrl: './task-list.component.css'
 })
 export class TaskListComponent {
+  tasks: Task[] = [
+    {
+      id: 1,
+      name: "Design wireframe",
+      description: "",
+      completed: false,
+      dueDate: new Date("2024-12-12"),
+      project: 1
+    },
+    {
+      id: 2,
+      name: "Develop frontend",
+      description: "",
+      completed: true,
+      dueDate: new Date("2024-10-10"),
+      project: 1
+    },
+    {
+      id: 3,
+      name: "Solve poverty",
+      description: "",
+      completed: false,
+      dueDate: new Date("2024-12-31"),
+      project: 1
+    },
+    {
+      id: 4,
+      name: "Have a party",
+      description: "",
+      completed: true,
+      dueDate: new Date("2025-01-01"),
+      project: 1
+    }
+  ];
 
+  handleCheckbox(id: number) {
+    const taskIndex: number = this.tasks.findIndex((task) => task.id === id);
+    this.tasks[taskIndex].completed = !this.tasks[taskIndex].completed;
+  }
 }
