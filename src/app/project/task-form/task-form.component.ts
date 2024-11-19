70393546274ce7383a35fc26ff4d9fe68fa048f9
Egor Akimov <akimoved@gmail.com>
2024-11-20 00:17:49 +0400

    add task form create task capabilities

--- src/app/project/task-form/task-form.component.ts
+++ src/app/project/task-form/task-form.component.ts
@@ -1,12 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, inject, Output } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Task } from '../../task.model';
+import { TaskService } from '../../task.service';
 
 @Component({
   selector: 'app-task-form',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './task-form.component.html',
   styleUrl: './task-form.component.css'
 })
 export class TaskFormComponent {
+  @Output() closePanel = new EventEmitter<'SUBMIT'>();
 
+  taskForm: FormGroup;
+
+  private taskService: TaskService = inject(TaskService);
+
+  constructor(private fb: FormBuilder) {
+    this.taskForm = this.fb.group({
+      name: ['', Validators.required],
+      description: [''],
+      dueDate: ['', Validators.required],
+      project: [0]
+    });
+  }
+
+  handleSubmit() {
+    if(this.taskForm.valid) {
+      const newTask: Task = {
+        ...this.taskForm.value,
+        dueDate: new Date(this.taskForm.value.dueDate),
+        completed: false
+      };
+
+      this.taskService.addTask(newTask);
+      this.closePanel.emit('SUBMIT');
+    }
+  }
 }
