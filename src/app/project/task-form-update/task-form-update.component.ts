905ed81256642a530fb89c494c8160045dc39622
Egor Akimov <akimoved@gmail.com>
2024-11-29 23:00:50 +0400

    add separate update form

---
+++ src/app/project/task-form-update/task-form-update.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-task-form-update',
+  standalone: true,
+  imports: [],
+  templateUrl: './task-form-update.component.html',
+  styleUrl: './task-form-update.component.css'
+})
+export class TaskFormUpdateComponent {
+
+}
